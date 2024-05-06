fd6267b80674813a890f7889cbdc7ca5f28f961d
shivpalSW <shivpal.wadkar2050@gmail.com>
2024-05-06 15:10:09 +0530

    init_setup.sh file updated with python 3.10

--- init_setup.sh
+++ init_setup.sh
@@ -1,7 +1,7 @@
 echo [$(date)]: "START"
-echo [$(date)]: "Creating a conda env with python 3.9"
-conda create --prefix ./chainlitenv python=3.9 -y
-echo [$(date)]: "Created conda environmnt with python 3.9"
+echo [$(date)]: "Creating a conda env with python 3.10"
+conda create --prefix ./chainlitenv python=3.10 -y
+echo [$(date)]: "Created conda environmnt with python 3.10"
 source activate ./chainlitenv
 echo [$(date)]: "Installing the Requirements python"
 pip install -r requirements.txt
