f116a1d0685a258bd179f0989031e845eff2142b
shivpalSW <shivpal.wadkar2050@gmail.com>
2024-05-06 17:00:46 +0530

    app.py updated

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 import chainlit as cl
-
+from src.llm import ask_bot
+from src.config import instruction
 
 @cl.on_message
-async def main(message: cl.Message):
-    # Your custom logic goes here...
+async def main(user_message:cl.Message):
 
+    response=ask_bot(user_message.content,instruction)
+    
     # Send a response back to the user
     await cl.Message(
-        content=f"Received: {message.content}",
+        content=f"Received: {response}",
     ).send()
+
